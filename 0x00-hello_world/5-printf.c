bcaeff7c15b69b8942dd89174a3975a8e72d3967
Charity <charity.softwaredev@gmail.com>
2023-02-16 09:06:29 -0800

    program that prints exactly with proper grammar, but the outcome is a piece of art, followed by a new line

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * Main - intiates a string
- * print - with proper grammar, but the outcome is a piece of art,
- * return - 0
+ * main - Print a string
+ * Description: use printf
+ * Return: 0
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0); 
+	return (0);
 }
