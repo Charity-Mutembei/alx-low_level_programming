24b3dcba13d1029bee53ace179fafbe7ee3fdcdc
Charity <charity.softwaredev@gmail.com>
2023-02-16 11:45:36 -0800

    C program that prints exactly the provided string and give a standard erro

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - extry point
+ * message - and that piece of art is useful\" - Dora Korpar, 2015-10-19
+ * return - 1
+ */
+int main() {
+    const char message[] = "and that piece of art is useful\" - Dora Korpar,				 2015-10-19\n";
+    write(STDOUT_FILENO, message, sizeof(message) - 1);
+    return 1;
+}
