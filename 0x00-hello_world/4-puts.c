60a7df5b28eb2ad5b4e6d376bfc93bcc0755535f
Charity <charity.softwaredev@gmail.com>
2023-02-16 08:17:20 -0800

    program that prints exactly - Programming is like building a multilingual puzzle followed y a new line and not use print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**  This is the right way to write a print function using puts
+* puts is an alternative of printf in C
+* When using the Betty style, the function is in its independent line 
+* after the curly braces and the return 0 is in brackets too
+*/
 
 int main(void)
 {
