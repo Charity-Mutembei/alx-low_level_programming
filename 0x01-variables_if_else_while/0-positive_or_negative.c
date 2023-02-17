1847f5e3a988b100037187b1fee1982735ee5799
Charity <charity.softwaredev@gmail.com>
2023-02-17 12:30:26 -0800

    program showing if the n is positive or negative or is zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,25 +5,26 @@
  * added header for printf
  * main - Entry point
  * int - n
- * if statement checking if the number is positive or not*/
+ * if statement checking if the number is positive or not
+ */
 
 int main(void)
 {
 	int n;
 
-	srand(time(0));
+	srand (time(0));
 	n = rand() - RAND_MAX / 2;
 	if(n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
+		{
+			printf("%d is positive\n", n);
+			}
 	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
+		{
+			printf("%d is zero\n", n);
+			}
 	else
-       	{
-		printf("%d is negative\n", n);
-	}
+		{
+			printf("%d is negative\n", n);
+		}
 	return (0);
 }
