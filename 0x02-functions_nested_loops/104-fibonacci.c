dfe6998824b64a188fc2cc4d88b7ba0bd6074912
Charity K. Mutembei <100141458+Charity-Mutembei@users.noreply.github.com>
2023-02-22 21:42:26 +0300

    program that finds and prints the first 98 fibonnanci numbers starting with 1 and 2, followed by new line and a coma between the numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - prints out first 98
+ * *fibonacci suit numbers
+ * *Return: return 0
+ */
+int main(void)
+{
+int nr = 0, n = 1, n1 = 2 , n3;
+
+while (nr <= 98)
+{
+n3 = n + n1;
+	
+printf("%d", n3);
+nr++;
+
+n = n1;
+n1 = n3;
+
+if (nr == 98)
+{
+printf("\n");
+}
+else
+{
+printf(",");
+}
+}
+return (0); 
+}
