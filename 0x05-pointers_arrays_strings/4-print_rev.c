80c6a8fb76ae71fce2b2772b4b3a7e04c8d70961
Charity K. Mutembei <100141458+Charity-Mutembei@users.noreply.github.com>
2023-02-28 00:55:47 +0300

    function that prints a string in reverse followerd by new line

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,6 +9,7 @@ void print_rev(char *s)
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++);
+	/** we declare the string above and use it below to reverse*/
 	for (i = i - 1; s[i] != '\0'; i--)
 	{
 		_putchar(s[i]);
