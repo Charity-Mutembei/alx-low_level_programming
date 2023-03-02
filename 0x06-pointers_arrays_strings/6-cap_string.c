7a3d4b9e364b073cd752568a723682e7566559f1
Charity K. Mutembei <100141458+Charity-Mutembei@users.noreply.github.com>
2023-03-03 01:28:49 +0300

    function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,31 +2,25 @@
 /**
  *cap_string - capitalizes every first letter of a word in a string.
  *separators of words are:  space, tabulation,
- * new line, ,, ;, ., !, ?, ", (, ), {, and }.
- *@s: pointer to string.
  *Return: pointer to s.
  */
 char *cap_string(char *str)
 {
-int i = 0;
-int capitalize_next = 1;
+	    int i = 0;
+	        int capitalize_next = 1;
 
-while (str[i] != '\0')
-{
-if (capitalize_next && str[i] >= 'a' && str[i] <= 'z')
-{
-str[i] -= ('a' - 'A');
-capitalize_next = 0;
-}
-else if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-str[i] == '(' || str[i] == ')' || str[i] == '{' ||
-str[i] == '}')
-{
-capitalize_next = 1;
-}
-i++;
-}
-return (str);
+		    while (str[i] != '\0') {
+			            if (capitalize_next && str[i] >= 'a' && str[i] <= 'z') {
+					                str[i] -= ('a' - 'A');
+							            capitalize_next = 0;
+								            } else if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+											                       str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+													                          str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+																                     str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+																		                        str[i] == '}') {
+										                capitalize_next = 1;
+												        }
+				            i++;
+					        }
+		        return (str);
 }
