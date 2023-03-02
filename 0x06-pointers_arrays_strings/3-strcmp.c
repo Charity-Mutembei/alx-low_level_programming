e73cf47a61e41775dcc79668ecbc23281bf883f3
Charity K. Mutembei <100141458+Charity-Mutembei@users.noreply.github.com>
2023-03-02 15:26:24 +0300

    function that compares two strings, prototype provided and function expected to work exactly like strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,13 +9,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int counter, cmpVal;
-counter = 0;
+	int counter, cmpVal;
 
-while (s1[counter] == s2[counter] && s1[counter] != '\0')
-{
-counter++;
-}
-cmpVal = s1[counter] - s2[counter];
-return (cmpVal);
+	counter = 0;
+	while (s1[counter] == s2[counter] && s1[counter] != '\0')
+		{
+			counter++;
+		}
+	cmpVal = s1[counter] - s2[counter];
+	return (cmpVal);
 }
