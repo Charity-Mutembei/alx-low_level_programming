b1d0256c25cfcd54c037160ead5076304c772149
Charity K. Mutembei <100141458+Charity-Mutembei@users.noreply.github.com>
2023-03-07 00:42:47 +0300

    function that fills memory with a constant byte

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MINA_H
+#define MAIN_H
+/**
+ *  * main - header file for prototype
+ *   * void prototypes (void)
+ *    * int prototypes (int)
+ */
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
