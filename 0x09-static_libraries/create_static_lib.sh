4a97808c7fc0a3668b90f9471ddc998595d4a86c
Charity K. Mutembei <100141458+Charity-Mutembei@users.noreply.github.com>
2023-03-10 19:19:11 +0300

    script that is called create_static_lib.sh that creates a static library called liball.a from all the .c files that are in the current directory

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,21 +1,4 @@
-#!/bin/bash 
-
-# collecting all .c files in the current directory
-SOURCES = $(ls *.c)
-
-#compile each of the source files found in object files
-for SOURCE in SOURCES
-do
-	out = "${SOURCE%.c}.o"
-	gcc -c $SOURCE -o $out
-done
-
-#create the static library from the objects files above
+#!/bin/bash
+gcc -Wall -pedantic -Werror -Wextra -c *.c
 ar rc liball.a *.o
-
-#then we can index each object file received 
-ranlib liball.a 
-#the only remaining step is that of using the library in a program 
-
-#clean up the objects files if you want 
-#here is the command - rm *.o
+ranlib liball.a
