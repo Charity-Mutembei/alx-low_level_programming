639d689ce12441a9e835272293b1dbfccb1796a6
Charity K. Mutembei <100141458+Charity-Mutembei@users.noreply.github.com>
2023-03-11 02:56:20 +0300

    script correction

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
- * main - print the name of the executable.
- * @argc: argument count or size of arguments.
- * @argv: argument vector
- * Return: always 0.
+ * main - Prints the name of the program
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
  */
 int main(int argc __attribute__((unused)), char **argv)
 {
