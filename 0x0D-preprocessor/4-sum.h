d10bdadcaeb7e504eb0498daa9e393513e9e2980
Charity K. Mutembei <100141458+Charity-Mutembei@users.noreply.github.com>
2023-03-20 22:00:08 +0300

    function like macro SUM (x,y) that computes the numbers x and y

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,6 +1,5 @@
 #ifndef _OBJECT_LIKE_MACRO_
 #define _OBJECT_LIKE_MACRO_
 
-#define SUM(x, y) (((x) < (0) ? ((x) * (-1)) : (x)) + ((y) < (0) ? ((y) * (-1)) : (y)))
-
+#define SUM(x, y) ((x) + (y))
 #endif
