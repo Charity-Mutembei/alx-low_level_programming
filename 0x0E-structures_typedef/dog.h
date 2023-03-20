508ef53f31bd4912a61cb596e551ce19e4783dc7
Charity K. Mutembei <100141458+Charity-Mutembei@users.noreply.github.com>
2023-03-20 22:29:17 +0300

    a new type struct dog with the folowing elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _OBJECT_LIKE_MACRO_
+#define _OBJECT_LIKE_MACRO_
+/**
+ * struct dog - data structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Description: dog with name, age, and owner's name.
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+}
+dog;
+
+#endif
