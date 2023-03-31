83a95d43a0c46b140dfc80b193287a9580b5276d
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-03-31 05:48:17 +0300

    function that prinnts the provided string

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "lists.h"
-void before_main(void) __attribute__ ((constructor));
 /**
  *before_main - prints a msg before the main
  * main - check the code *
@@ -10,5 +9,4 @@ void before_main(void)
 {
 printf("You're beat! and yet, you must allow,\n"
 "I bore my house upon my back!\n");
-return (0);
 }
