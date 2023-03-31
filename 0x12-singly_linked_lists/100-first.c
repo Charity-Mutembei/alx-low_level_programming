7773bb0fb23125ccc82d28310bfc56ab8da0e956
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-03-31 05:42:12 +0300

    function that prinnts the provided string

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ *before_main - prints a msg before the main
+ * main - check the code *
+ * Return: Always 0.
+ */
+int before_main(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+return (0);
+}
