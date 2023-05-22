13720ca69a4a4ef4b4e295737b6d3090c053e2c4
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-05-23 01:08:40 +0300

    Task 2: Lets call C functions from Python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,55 @@
+/**
+ * Function: add
+ * Adds two integers.
+ * Parameters:
+ * - a: First integer
+ * - b: Second integer
+ * Returns: - Sum of the two integers
+*/
+int add(int a, int b) {
+    return a + b;
+}
+/* Function: sub
+   Subtracts two integers.
+   Parameters:
+     - a: First integer
+     - b: Second integer
+   Returns:
+     - Difference of the two integers
+*/
+int sub(int a, int b) {
+    return a - b;
+}
+/* Function: mul
+   Multiplies two integers.
+   Parameters:
+     - a: First integer
+     - b: Second integer
+   Returns:
+     - Product of the two integers
+*/
+int mul(int a, int b) {
+    return a * b;
+}
+/* Function: div
+   Divides two integers.
+   Parameters:
+     - a: First integer
+     - b: Second integer
+   Returns:
+     - Quotient of the two integers
+*/
+int div(int a, int b) {
+    return a / b;
+}
+/* Function: mod
+   Computes the modulo of two integers.
+   Parameters:
+     - a: First integer
+     - b: Second integer
+   Returns:
+     - Modulo of the two integers
+*/
+int mod(int a, int b) {
+    return a % b;
+}
