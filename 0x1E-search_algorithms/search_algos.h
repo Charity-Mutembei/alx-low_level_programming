2ddda51fa3d66262d5f5b4672df1c8af9b2f2b11
Charity Mutembei <charity.softwaredev@gmail.com>
2023-09-13 23:19:50 +0300

    edited: search_algos.h 3 file(s)  created:  Pushed at 2023-09-13 23:...

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -12,10 +12,12 @@
  * linear_search - entry point for prototype
  * binary_search - entry point for proptoype 2
  * jump_search - entry point for prototype 3
+ * interpolation_search - entry point for prototype 4
  * Return: -1 for fail
  */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif
