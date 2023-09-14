0612d923e83c8dc963718cf01603c81809b792a9
Charity Mutembei <charity.softwaredev@gmail.com>
2023-09-14 05:22:19 +0300

    edited: 103-exponential.c 2 file(s)  created:  Pushed at 2023-09-14 ...

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,4 +1,45 @@
-#include "search_algos.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * binary_search - Entry point
+ * int binary_search(int *array, size_t size, int value) - prototype
+ * @array: pointer and array
+ * @size: the n of the array
+ * @value: that we are looking for
+ * Return: -1 for no success
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+size_t left = 0;
+size_t right = size - 1;
+size_t i;
+
+if (array == NULL || size == 0)
+return (-1);
+
+while (left <= right)
+{
+int mid = (right + left) / 2;
+printf("Searching in array: %d", array[left]);
+for (i = left + 1; i <= right; i++)
+{
+printf(", %d", array[i]);
+}
+printf("\n");
+
+if (array[mid] == value)
+return (mid);
+else if (array[mid] < value)
+left = mid + 1;
+else
+right = mid - 1;
+}
+
+return (-1);
+}
 /**
  * exponential_search - entry point for prototype
  * @array: our array
@@ -29,5 +70,5 @@ bound *= 2;
 
 printf("Value found between indexes [%d] and [%d]\n", low, high);
 
-return (binary_search(array, low, high, value));
+return (binary_search(array, low, high));
 }
